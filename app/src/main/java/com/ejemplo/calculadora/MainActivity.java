fe9b716213f728fc12e9cedf1cfcc4c684fbbee7
EstebanIslas <1717110261@utectulancingo.edu.mx>
2019-02-28 20:15:33 -0600

    Calculadora V0.1

---
+++ app/src/main/java/com/ejemplo/calculadora/MainActivity.java
@@ -0,0 +1,94 @@
+package com.ejemplo.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etxt_num1;
+    EditText etxt_num2;
+    EditText etxt_resultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etxt_num1 = (EditText)findViewById(R.id.etxt_num1);
+        etxt_num2 = (EditText)findViewById(R.id.etxt_num2);
+        etxt_resultado = (EditText)findViewById(R.id.etxt_resultado);
+    }
+
+    public void sumarOnClick(View view){
+        try{
+            double numero1 = Double.parseDouble(etxt_num1.getText().toString());
+            double numero2 = Double.parseDouble(etxt_num2.getText().toString());
+
+            double resultado = numero1 + numero2;
+
+            etxt_resultado.setText(String.valueOf(resultado));
+        }catch (Exception ex){
+            Log.e("Advertencia!", "Accion no valida!!", ex);
+        }
+
+    }
+
+    public void restarOnClick(View view){
+        try {
+            double numero1 = Double.parseDouble(etxt_num1.getText().toString());
+            double numero2 = Double.parseDouble(etxt_num2.getText().toString());
+
+            double resultado = numero1 - numero2;
+
+            etxt_resultado.setText(String.valueOf(resultado));
+        }catch (Exception ex){
+            Log.e("Advertencia!", "Accion no valida!!", ex);
+        }
+    }
+
+    public void multiplicarOnClick(View view){
+        try{
+            double numero1 = Double.parseDouble(etxt_num1.getText().toString());
+            double numero2 = Double.parseDouble(etxt_num2.getText().toString());
+
+            double resultado = numero1 * numero2;
+
+            etxt_resultado.setText(String.valueOf(resultado));
+        }catch (Exception ex){
+            Log.e("Advertencia!", "Accion no valida!!", ex);
+        }
+    }
+
+    public void dividirOnClick(View view){
+        try {
+
+            double numero1 = Double.parseDouble(etxt_num1.getText().toString());
+            double numero2 = Double.parseDouble(etxt_num2.getText().toString());
+
+            double resultado = numero1 / numero2;
+
+            etxt_resultado.setText(String.valueOf(resultado));
+
+        }catch (Exception ex){
+            Log.e("Advertencia!", "Accion no valida!!", ex);
+        }
+    }
+
+    public void moduloOnClick(View view){
+        try {
+
+            double numero1 = Double.parseDouble(etxt_num1.getText().toString());
+            double numero2 = Double.parseDouble(etxt_num2.getText().toString());
+
+            double resultado = numero1 % numero2;
+
+            etxt_resultado.setText(String.valueOf(resultado));
+
+        }catch (Exception ex){
+            Log.e("Advertencia!", "Accion no valida!!", ex);
+        }
+    }
+}
